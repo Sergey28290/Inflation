9d918e7d17f1ce08ba9f0c81012685469dd02162
Sergey28290 <77580966+Sergey28290@users.noreply.github.com>
2021-05-09 21:37:13 +0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -4,29 +4,27 @@ import csv
 
 
 def inflation_view(request):
-    readers_keys = []  # массив для шапки таблицы
-    readers_items = []  # создаем массив для значений инфляции
-    dir = settings.BASE_DIR + '/inflation_russia.csv'  # переходим в корневую папку и находим там файл
-    # dir = 'C:/Users/serge/Documents/PYTHON/Django/Inflation/inflation_russia.csv'
-    with open(dir, encoding='utf-8') as csvfile:  # и открываем файл на чтение с кодировкой, т.к. текст русский
+    readers_keys = []  
+    readers_items = []  
+    dir = settings.BASE_DIR + '/inflation_russia.csv'  
+    with open(dir, encoding='utf-8') as csvfile: 
         reader = csv.DictReader(csvfile,
-                                delimiter=';')  # записываем результат в виде множества словарей { {'...': ...} }
-        for row in reader:  # перебираем его
+                                delimiter=';') 
+        for row in reader: 
             reader_keys_key = []
-            readers_items_item = []  # список для занесения значений инфляции
-            for keys, item in row.items():  # перебираем словари, метода items(), возвращает список пар «ключ—значение»
-                reader_keys_key.append(keys)  # добавляем в список шапку таблицы
-                try:  # пытаемся добавить в этот список значение, приобразованное в int
-                    readers_items_item.append(int(item))  # передаем года, т.к. они типа int
+            readers_items_item = []  
+            for keys, item in row.items():  
+                reader_keys_key.append(keys)  
+                try:  
+                    readers_items_item.append(int(item))  
                 except ValueError:
-                    try:  # пытаемся добавить в список значение приобразованное в float
-                        readers_items_item.append(float(item))  # передаем значение инфляции, т.к. они типа float
+                    try:  
+                        readers_items_item.append(float(item))  
                     except ValueError:
-                        readers_items_item.append("")  # и пустые значение оставляем пустыми
-            readers_items.append(readers_items_item)  # добавляем это все в общий список
+                        readers_items_item.append("")  
+            readers_items.append(readers_items_item)  
             readers_keys.append(reader_keys_key)
     template_name = 'inflation.html'
-    # чтение csv-файла и заполнение контекста
     context = {
         'csv_reader_keys': readers_keys,
         'csv_reader_items': readers_items
